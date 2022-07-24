ac839c2dfa5c0cfd024838fddb436f2dc1ff6788
mooooooi <emwings@outlook.com>
2022-07-24 22:26:17 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import * as core from "@actions/core";
+import { genToken } from "./token";
+import { upload } from "./upload";
+
+async function run(): Promise<void> {
+  try {
+    const ak = core.getInput("access_key");
+    const sk = core.getInput("secret_key");
+    const bucket = core.getInput("bucket");
+    const sourceDir = core.getInput("source_dir");
+    const destDir = core.getInput("dest_dir");
+    const ignoreSourceMap = core.getInput("ignore_source_map") === "true";
+
+    const token = genToken(bucket, ak, sk);
+
+    upload(
+      token,
+      sourceDir,
+      destDir,
+      ignoreSourceMap,
+      (file, key) => core.info(`Success: ${file} => [${bucket}]: ${key}`),
+      () => core.info("Done!"),
+      (error) => core.setFailed(error.message)
+    );
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
---
+++ src/upload.ts
@@ -0,0 +1,62 @@
+import qiniu from "qiniu";
+import path from "path";
+import glob from "glob";
+import pAll from "p-all";
+import pRetry from "p-retry";
+import fs from "fs";
+
+function normalizePath(input: string): string {
+  return input.replace(/^\//, "");
+}
+
+export function upload(
+  token: string,
+  srcDir: string,
+  destDir: string,
+  ignoreSourceMap: boolean,
+  onProgress: (srcFile: string, destFile: string) => void,
+  onComplete: () => void,
+  onFail: (errorInfo: any) => void
+): void {
+  let baseDir = path.resolve(process.cwd(), srcDir);
+  let files: string[];
+  if (fs.lstatSync(baseDir)?.isDirectory()) {
+    files = glob.sync(`${baseDir}/**/*`, { nodir: true });
+  } else {
+    files = [baseDir];
+    baseDir = process.cwd();
+  }
+
+  const config = new qiniu.conf.Config();
+  const uploader = new qiniu.form_up.FormUploader(config);
+
+  const tasks = files
+    .map((file) => {
+      const relativePath = path.relative(baseDir, path.dirname(file));
+      const key = normalizePath(
+        path.join(destDir, relativePath, path.basename(file))
+      );
+
+      if (ignoreSourceMap && file.endsWith(".map")) return null;
+
+      const task = (): Promise<any> =>
+        new Promise((resolve, reject) => {
+          const putExtra = new qiniu.form_up.PutExtra();
+          uploader.putFile(token, key, file, putExtra, (err, body, info) => {
+            if (err) return reject(new Error(`Upload failed: ${file}`));
+
+            if (info.statusCode === 200) {
+              onProgress(file, key);
+              return resolve({ file, to: key });
+            }
+
+            reject(new Error(`Upload failed: ${file}`));
+          });
+        });
+
+      return () => pRetry(task, { retries: 3 });
+    })
+    .filter((item) => !!item) as (() => Promise<any>)[];
+
+  pAll(tasks, { concurrency: 5 }).then(onComplete).catch(onFail);
+}
